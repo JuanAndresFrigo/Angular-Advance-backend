909cb2c8754dd933fb751e46d7610a54f0c967eb
Juan Andres Frigo <juan.a.frigo@gmail.com>
2023-01-15 15:18:13 -0300

    Fin sección 11

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -4,12 +4,20 @@ const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
 
 const getUsuarios = async (req, res) => {
-  const usuario = await Usuario.find({}, "nombre email role google");
+  // Parametro de la request
+  const desde = Number(req.query.desde || 0);
+
+  // Ejecuto ambas promesas al mismo tiempo
+  const [usuarios, total] = await Promise.all([
+    Usuario.find({}, "nombre email role google img").skip(desde).limit(5),
+
+    Usuario.countDocuments(),
+  ]);
 
   res.json({
     ok: true,
-    usuario,
-    uid: req.uid,
+    usuarios,
+    total,
   });
 };
 
---
+++ helpers/actualizar-imagen.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+
+const Usuario = require("../models/usuario");
+const Medico = require("../models/medico");
+const Hospital = require("../models/hospital");
+const hospital = require("../models/hospital");
+
+const borrarImagen = (path) => {
+  if (fs.existsSync(path)) {
+    //Borrar la imagen anterior
+    fs.unlinkSync(path);
+  }
+};
+
+const actualizarImagen = async (tipo, id, nombreArchivo) => {
+  let pathViejo = "";
+  switch (tipo) {
+    case "medicos":
+      const medico = await Medico.findById(id);
+      if (!medico) {
+        console.log("No se encontró médico por id");
+        return false;
+      }
+
+      pathViejo = `./uploads/medicos/${medico.img}`;
+      borrarImagen(pathViejo);
+
+      medico.img = nombreArchivo;
+      medico.save();
+      return true;
+
+      break;
+    case "hospitales":
+      const hospital = await Hospital.findById(id);
+      if (!hospital) {
+        console.log("No se encontró hospital por id");
+        return false;
+      }
+
+      pathViejo = `./uploads/hospitales/${hospital.img}`;
+      borrarImagen(pathViejo);
+
+      hospital.img = nombreArchivo;
+      hospital.save();
+      return true;
+      break;
+    case "usuarios":
+      const usuario = await Usuario.findById(id);
+      if (!usuario) {
+        console.log("No se encontró usuario por id");
+        return false;
+      }
+
+      pathViejo = `./uploads/usuarios/${usuario.img}`;
+      borrarImagen(pathViejo);
+
+      usuario.img = nombreArchivo;
+      usuario.save();
+      return true;
+      break;
+
+    default:
+      break;
+  }
+};
+
+module.exports = {
+  actualizarImagen,
+};
