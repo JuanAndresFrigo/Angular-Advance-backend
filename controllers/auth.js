9f80cb85beea6d6ad5fcb15d878cc7078639fb6d
Juan Andres Frigo <juan.a.frigo@gmail.com>
2023-01-15 22:59:28 -0300

    Fin seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require("bcryptjs");
 
 const Usuario = require("../models/usuario");
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
@@ -45,6 +46,49 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  try {
+    const { email, name, picture } = await googleVerify(req.body.token);
+
+    // Verificar si ya existe usuario con el email
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+
+    if (!usuarioDB) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: "@@@",
+        img: picture,
+        google: true,
+      });
+    } else {
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    await usuario.save();
+
+    // Generar token
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      email,
+      name,
+      picture,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      ok: false,
+      msg: "Token de Google no es correcto",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
